507e7935a8266a6a3c44c1c2e73f077210ba3639
Cass <160861257+Rockyouge@users.noreply.github.com>
2024-06-04 10:34:56 -0300

    calcu.py

--- calcu.py
+++ calcu.py
@@ -1,19 +1,16 @@
-from calculadora import calculadora
-
-n1 = float(input("Diga um número: "))
-n2 = float(input("Diga mais um número: "))
-print("operações: somar, subtrair, multiplicar")
-
-op = input("Escreva o nome da operação desejada: ")
-
-calc = calculadora()
-
-if op == "somar":
-    print(calc.somar(n1, n2))
-elif op == "subtrair":
-    print(calc.subtrair(n1, n2))
-elif op == "multiplicar":
-    print(calc.multiplicar(n1, n2))
-else:
-    print("Operação inválida")
-
+class calculadora:
+    def soma (self,n1,n2):
+        result=n1+n2
+        return(result)
+   
+    def subtrair(self,n1,n2):
+        result = n1 - n2
+        return(result)
+ 
+    def multiplicar(self,n1,n2):
+        result = n1 * n2
+        return(result)
+   
+    def dividir(self,n1,n2):
+        result = n1 / n2
+        return(result)
